26a345e4e6388074293f1068cba52ad466194780
simplekind <86557655+simplekind@users.noreply.github.com>
2021-10-30 17:40:08 +0530

    Updated some server changes

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,4 @@
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const myPeer = new Peer(undefined,{
+//     host:'/',
+//     port:'3001'
+// })
